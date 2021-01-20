ba0c907c9add8fdd2713fefc0629b607e963b2b8
Erik J Brown <erkjbro@gmail.com>
2021-01-20 14:22:08 -0600

    al2 httpd userdata added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # scripts
 A collection of scripts that are stored together for easy access. Mainly for env setups.
+
+## VM
+
+### Amazon Linux 2
+
+- basic-httpd.sh
+
+This script was in an AWS course that I'm taking on Udemy. It's a simple way to get something running
+on the ec2 instance that can help with testing load balancing, acs, alb, etc. Nothing fancy.
---
+++ vm/amazon-linux-2/basic-httpd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+########################################################
+##### USE THIS FILE IF YOU LAUNCHED AMAZON LINUX 2 #####
+########################################################
+
+# get admin privileges
+sudo su
+
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd.service
+systemctl enable httpd.service
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h1>Hello World from $(hostname -f) in AZ $EC2_AVAIL_ZONE </h1>" > /var/www/html/index.html
